e78db27d06ebb99e31f90512239c5bb0ffb8e8fc
KyungHoon <kidi052812@gmail.com>
2020-07-31 19:35:50 +0900

    [INIT] : INITIAL
    Create Project

---
+++ XKCD-Viewer/XKCD-Viewer/XKCD/ApiHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XKCD_Viewer.XKCD
+{
+    class ApiHelper
+    {
+    }
+}
